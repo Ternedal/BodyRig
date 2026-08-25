83631a11a517ab1780dfaaeb72b2820a41dafe19
Ternedal <a.nielbaek@gmail.com>
2026-08-25 17:10:58 +0200

    Test scoped native stderr handling

--- tests/test_openpose_cuda_root_contract.py
+++ tests/test_openpose_cuda_root_contract.py
@@ -18,13 +18,13 @@ def test_openpose_setup_binds_explicit_cuda_root() -> None:
     assert 'which", "nvcc"' not in script
 
 
-def test_openpose_wsl_capture_uses_process_exit_code_not_powershell_native_error_stream() -> None:
+def test_openpose_wsl_capture_uses_exit_code_without_runtime_specific_argument_list() -> None:
     script = _script()
 
-    assert '[System.Diagnostics.ProcessStartInfo]::new()' in script
-    assert '$startInfo.RedirectStandardOutput = $true' in script
-    assert '$startInfo.RedirectStandardError = $true' in script
-    assert '$exitCode = $process.ExitCode' in script
-    assert 'Stdout = $stdout' in script
-    assert 'Stderr = $stderr' in script
-    assert '$output = & $WslExe' not in script
+    assert '$ErrorActionPreference = "Continue"' in script
+    assert '$PSNativeCommandUseErrorActionPreference = $false' in script
+    assert '$output = & $WslExe -d $Distribution -- @Arguments 2>&1' in script
+    assert '$exitCode = $LASTEXITCODE' in script
+    assert 'ExitCode = $exitCode' in script
+    assert '[System.Diagnostics.ProcessStartInfo]::new()' not in script
+    assert '.ArgumentList' not in script
