393bdc87358a458b5a089aee3538f0603a73acbc
Ternedal <a.nielbaek@gmail.com>
2026-08-25 21:00:26 +0200

    Test PHALP recovery runtime dependency boundary

--- tests/test_recovery_setup_staged_install_contract.py
+++ tests/test_recovery_setup_staged_install_contract.py
@@ -25,12 +25,25 @@ def test_recovery_conda_base_is_staged_before_pip_source_builds() -> None:
     assert '@("env", "create"' not in text
 
 
-def test_editable_recovery_installs_disable_build_isolation() -> None:
+def test_source_builds_disable_build_isolation() -> None:
     text = _script()
 
-    four_d_install = '@("-m", "pip", "install", "--disable-pip-version-check", "--no-build-isolation", "-e", $fourDPath)'
-    phalp_install = '@("-m", "pip", "install", "--disable-pip-version-check", "--no-build-isolation", "-e", $phalpPath)'
-
-    assert four_d_install in text
-    assert phalp_install in text
+    assert '"--no-build-isolation", "-e", $fourDPath' in text
+    assert '"detectron2 @ git+https://github.com/facebookresearch/detectron2.git"' in text
+    assert '"--no-build-isolation", "--no-deps", "-e", $phalpPath' in text
     assert "Detectron2 imports Torch from setup.py" in text
+
+
+def test_windows_recovery_omits_unused_phalp_neural_renderer() -> None:
+    text = _script()
+
+    assert 'render.enable=false' in text
+    assert 'install the pinned PHALP checkout with --no-deps' in text
+    assert '"scenedetect[opencv]"' in text
+    assert '"pyopengl @ git+https://github.com/mmatl/pyopengl.git"' in text
+    assert '"chumpy @ git+https://github.com/mattloper/chumpy"' in text
+    assert 'neural-renderer-pytorch' in text  # rationale comment only
+
+    command_section = text[text.index('Invoke-Checked -Executable $envPython -Arguments @(', text.index('Install pinned 4D-Humans checkout')):]
+    command_section = command_section[: command_section.index('$smplDestination')]
+    assert '"neural-renderer-pytorch' not in command_section
