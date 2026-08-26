dd6db30ecb3c2e63a40231e8f69fce9ebbc84d47
Ternedal <a.nielbaek@gmail.com>
2026-08-26 21:06:46 +0200

    Pin NMR runtime authority for HMR2 recovery

--- bodyrig/bridges/hmr2_config.py
+++ bodyrig/bridges/hmr2_config.py
@@ -6,8 +6,12 @@ FOUR_D_HUMANS_REVISION = "efe18deff163b29dff87ddbd575fa29b716a356c"
 PHALP_REVISION = "96f7e6c09fb858ec3f597d59246c151ab4394bc3"
 # Git blob identity of phalp/trackers/PHALP.py at the pinned PHALP revision.
 PHALP_TRACKER_BLOB_SHA1 = "f4258ab37f2cf034e7321f7ec48ef61be6001785"
+NMR_REVISION = "e990b3c70f48d39231f607c79d76ce3db4bf7483"
+NMR_REMOTE = "https://github.com/shubham-goel/NMR.git"
 ADAPTER_NAME = "4dhumans-hmr2-phalp"
-ADAPTER_REVISION = f"4dh:{FOUR_D_HUMANS_REVISION};phalp:{PHALP_REVISION}"
+ADAPTER_REVISION = (
+    f"4dh:{FOUR_D_HUMANS_REVISION};phalp:{PHALP_REVISION};nmr:{NMR_REVISION}"
+)
 
 
 def bridge_script_path() -> Path:
