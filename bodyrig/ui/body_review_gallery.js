686edb6bc1a08ee28d4188dacce1b29475093714
Ternedal <a.nielbaek@gmail.com>
2026-09-04 17:29:13 +0200

    feat: load hair promotion Person Studio layer

--- bodyrig/ui/body_review_gallery.js
+++ bodyrig/ui/body_review_gallery.js
@@ -206,3 +206,6 @@ void import("/ui/high_fidelity_component_review.js").catch((error) => {
 void import("/ui/high_fidelity_hair_deformation_review.js").catch((error) => {
   console.error("BodyRig high-fidelity hair-deformation-review UI could not be loaded", error);
 });
+void import("/ui/high_fidelity_hair_promotion.js").catch((error) => {
+  console.error("BodyRig high-fidelity hair-promotion UI could not be loaded", error);
+});
