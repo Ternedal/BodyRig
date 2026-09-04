2169af224a57f41590d9608b468441a7227a8594
Ternedal <a.nielbaek@gmail.com>
2026-09-04 23:44:58 +0200

    feat: load unified high-fidelity continuation UI

--- bodyrig/ui/body_review_gallery.js
+++ bodyrig/ui/body_review_gallery.js
@@ -209,3 +209,6 @@ void import("/ui/high_fidelity_hair_deformation_review.js").catch((error) => {
 void import("/ui/high_fidelity_hair_promotion.js").catch((error) => {
   console.error("BodyRig high-fidelity hair-promotion UI could not be loaded", error);
 });
+void import("/ui/high_fidelity_continuation.js").catch((error) => {
+  console.error("BodyRig unified high-fidelity continuation UI could not be loaded", error);
+});
