a331d3d9132a7e3e7e9eb3f72a85a4b7f0299383
Ternedal <a.nielbaek@gmail.com>
2026-09-15 22:09:55 +0200

    test(photoreal): guard OpenMMLab WSL install

--- tests/test_photoreal_reference_wsl_setup_operator.py
+++ tests/test_photoreal_reference_wsl_setup_operator.py
@@ -13,6 +13,22 @@ def test_reference_wsl_setup_pins_published_onnxruntime_gpu_wheel() -> None:
     assert '"onnxruntime-gpu==$onnxruntimeVersion"' in SCRIPT
 
 
+def test_reference_wsl_setup_keeps_numpy1_compatible_opencv() -> None:
+    assert '$numpyVersion = "1.26.4"' in SCRIPT
+    assert '$opencvVersion = "4.9.0.80"' in SCRIPT
+    assert '"opencv-python==$opencvVersion"' in SCRIPT
+    assert '"opencv-python-headless"' not in SCRIPT
+
+
+def test_reference_wsl_mmdetection_build_sees_installed_torch() -> None:
+    mmdet_url = '"git+https://github.com/open-mmlab/mmdetection.git@$mmdetRevision"'
+    mmdet_install = SCRIPT.index(mmdet_url)
+    no_isolation = SCRIPT.rindex('"--no-build-isolation"', 0, mmdet_install)
+    pip_install = SCRIPT.rindex('"pip", "install"', 0, no_isolation)
+
+    assert pip_install < no_isolation < mmdet_install
+
+
 def test_reference_wsl_force_repair_rebuilds_partial_environment() -> None:
     force_guard = SCRIPT.index("if ($Force)")
     remove_venv = SCRIPT.index('Invoke-Wsl -Root -Arguments @("/bin/rm", "-rf", $venvRoot)')
